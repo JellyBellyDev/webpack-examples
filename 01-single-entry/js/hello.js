a554a2675726d8274bd3189ccd483e171fdbb510
Andrea Giannantonio <a.giannantonio@gmail.com>
2016-02-01 09:10:17 +0100

    first example

---
+++ 01-single-entry/js/hello.js
@@ -0,0 +1,8 @@
+module.exports = {
+    hi: function (name) {
+        return 'Hi ' + name;
+    },
+    welcome: function (name) {
+        return 'Welcome ' + name;
+    }
+};
---
+++ 01-single-entry/webpack.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var path = require('path');
+
+// Our configuration
+module.exports = {
+
+    // Define the entry point
+    entry: path.resolve(__dirname, 'js', 'app.js'),
+
+    // Output configuration
+    output: {
+        path: path.resolve(__dirname, 'public', 'js'),
+        filename: 'bundle.js'
+    }
+};
