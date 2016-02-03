8f62d9d6c63ea911158b4ecf262030e0d52dd1bc
Andrea Giannantonio <andrea.giannantonio@immobiliare.it>
2016-02-03 15:42:26 +0100

    add 05 example.

---
+++ 05-linting/webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var path = require('path'),
+    webpack = require('webpack');
+
+// Our configuration
+module.exports = {
+
+    // Define the entry point
+    entry: path.resolve(__dirname, 'js', 'app.js'),
+
+    // Output configuration
+    output: {
+        path: path.resolve(__dirname, 'public', 'js'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        preLoaders: [
+            {test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/}
+        ]
+    },
+
+    resolve: {
+        modulesDirectories: [
+            'bower_components',
+            'node_modules',
+            'js/modules'
+        ]
+    }
+};
